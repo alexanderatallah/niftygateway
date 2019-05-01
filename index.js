bd2014bf6c604a584dff2aaa7314c901d9b77b91
Duncan Cock Foster <dcarlcf@gmail.com>
2019-05-01 10:01:49 -0700

    The NiftyGatewayJS package

---
+++ index.js
@@ -0,0 +1,5 @@
+//import NiftyGatewayJS from './NiftyGateway.js'
+Object.defineProperty(exports, "__esModule", { value: true });
+var nftg = require("./NiftyGateway");
+
+module.exports = nftg;
---
+++ window/windowFunctions.js
@@ -0,0 +1,16 @@
+
+
+export function messagePopUpWindow(popup, counter) {
+  //recursion
+  if (counter==10) {
+    return("done");
+  } else {
+    //wheeeeeee
+    setTimeout(function() {
+      console.log('sending message ' + counter);
+      popup.postMessage("ez_kitties_message", '*');
+      counter = counter+1;
+      return(messagePopUpWindow(popup, counter));
+    }, 1000)
+  }
+}
